8efcb2f9f25dadac67f6f52595c3e55e7c8518fd
yzg1943 <legecn@qq.com>
2016-07-02 18:33:25 +0800

    新增创建服务器

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var http = require('http');
-http.createServer(){
-
-}
+http.createServer(
+	function(req,res){
+		res.writeHead(200,{'Content-type':'text/plain'});
+		res.end('Hello Node.JS\n');
+	}
+).listen(1337,'127.0.0.1');
+console.log('server running at http://127.0.0.1:1337');
